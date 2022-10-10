a56750174468e395e287e69cb34299c037c43b46
Leandro Barbosa <leandro@indemnisation.com>
2022-10-10 16:01:52 +0200

    stacks and bio mobile version

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,17 @@
 import utilStyles from '../styles/utils.module.css';
+import Image from 'next/image';
 
 function Navbar() {
    return (
       <nav className={utilStyles.navbar}>
+           {/* <Image
+              priority
+              src="/images/lewagon.png"
+              className={utilStyles.wagonTop}
+              height={40}
+              width={70}
+              alt=""
+            /> */}
          <ul>
             <li className={utilStyles.active}>Stacks</li>
             <li>Contact</li>
