905c54225796d0195e0da90cabaeb009180c2de1
Leandro Barbosa <leandro@indemnisation.com>
2022-10-10 15:12:02 +0200

    stacks and bio mobile version

--- components/Contact.js
+++ components/Contact.js
@@ -3,7 +3,7 @@ import contactStyles from "../styles/contactStyles.module.css";
 function Contact() {
    return (
       <div className={contactStyles.contactBackground}>
-         <p><span className={contactStyles.contactLinkedin}>Linkedin</span></p>
+         <a href=" www.linkedin.com/in/helloleandro"><span className={contactStyles.contactLinkedin}>Linkedin</span></a>
          <p><span className={contactStyles.contactGithub}>Github</span></p>
          <p><span className={contactStyles.contactInstagram}>Instagram</span></p>
          <p><span className={contactStyles.contactMail}>Email</span></p>
