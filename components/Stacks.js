c46d17abfa8b3af364fbe63d47ea5de433eb2179
Leandro Barbosa <leandro@indemnisation.com>
2022-10-10 12:04:25 +0200

    stacks and bio mobile version

--- components/Stacks.js
+++ components/Stacks.js
@@ -6,11 +6,11 @@ function Stacks() {
          <h1><span className={stacksStyles.stacksTitle}>Stacks</span></h1>
          <span className={stacksStyles.stacksContent}>
             I'm a fullStack developer working mostly of my time with Ruby on Rails since the Bootcamp {'<Le Wagon />'}, but also I've working with all these stacks {'=>'}</span>
-         <div><span className={stacksStyles.stacksContentFramework}> as a frameworks: Rails | Synfony | React.js | Next.js</span></div>
-         <div><span className={stacksStyles.stacksContentFrontEnd}> as a frontend: HTML5 | CSS3 | JavaScript | jQuery</span></div>
-         <div><span className={stacksStyles.stacksContentBackEnd}> as a Backend: Ruby | Php | Node.js</span></div>
-         <div><span className={stacksStyles.stacksContentDataBase}> as a Database: PostgreSQL | Php.myAdmmin</span></div>
-         <div><span className={stacksStyles.stacksContentDesign}> as a Design: Figma and some Adobe Stuffs</span></div>
+         <div><span className={stacksStyles.stacksContentFramework}>Frameworks: Rails | Synfony | React.js | Next.js</span></div>
+         <div><span className={stacksStyles.stacksContentFrontEnd}>Frontend: HTML5 | CSS3 | JavaScript | jQuery</span></div>
+         <div><span className={stacksStyles.stacksContentBackEnd}>Backend: Ruby | Php | Node.js</span></div>
+         <div><span className={stacksStyles.stacksContentDataBase}>Database: PostgreSQL | Php.myAdmmin</span></div>
+         <div><span className={stacksStyles.stacksContentDesign}>Design: Figma and some Adobe Stuffs</span></div>
 
 
 
