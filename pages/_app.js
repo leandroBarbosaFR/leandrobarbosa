9381c3d8aaeef38f556d8faa30a4185d656d4fbf
Leandro Barbosa <leandro@indemnisation.com>
2022-10-10 11:27:59 +0200

    stacks and bio

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/global.css';
 import Navbar from "/components/Navbar.js";
+import Bio from "/components/Bio.js";
+import Stacks from "/components/Stacks.js";
 
 export default function App({ Component, pageProps }) {
    return <>
    <Navbar />
-   <Component {...pageProps} />;
+   <Component {...pageProps} />
+   <Bio />
+   <Stacks />;
    </>
  }
