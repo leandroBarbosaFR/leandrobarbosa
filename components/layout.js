0f0b4f76136af0bfbb52c3de0d52b1679b861ef6
Leandro Barbosa <leandro@indemnisation.com>
2022-10-10 12:30:29 +0200

    stacks and bio mobile version

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = "Hey! I'm Leandro";
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Leandro Barbosa';
 
 export default function Layout({ children, home }) {
   return (
